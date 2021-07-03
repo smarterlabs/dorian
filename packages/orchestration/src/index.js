7589a97165322a51d57edb441ce38055a179f863
Kennedy Rose <kennedyianrose@gmail.com>
2021-07-03 16:15:41 -0500

    netlify config

--- packages/orchestration/src/index.js
+++ packages/orchestration/src/index.js
@@ -1,9 +1,35 @@
 const download = require(`@app/downloader`)
 
-// Eventually make these env variables
-const siteUrl = `https://business-starter-template.webflow.io`
-const destinationOrigin = `https://smarterlabs.com/`
+// let siteUrl = `https://business-starter-template.webflow.io`
+// let destinationOrigin = `https://smarterlabs.com/`
+let siteUrl = process.env.SOURCE_URL
+let destinationOrigin = process.env.DESTINATION_URL
 
+// Exit if environment variables are missing
+if(!siteUrl){
+	console.error(`No "SOURCE_URL" environment variable set.`)
+	process.exit(1)
+}
+if(!destinationOrigin){
+	console.error(`No "DESTINATION_URL" environment variable set.`)
+	process.exit(1)
+}
+
+// Normalize links
+if(siteUrl.indexOf(`://`) === -1){
+	siteUrl = `https://` + siteUrl
+}
+while(siteUrl[siteUrl.length - 1] === `/`){
+	siteUrl = siteUrl.substring(0, siteUrl.length - 1)
+}
+if(destinationOrigin.indexOf(`://`) === -1){
+	destinationOrigin = `https://` + destinationOrigin
+}
+if(destinationOrigin[destinationOrigin.length - 1] !== `/`){
+	destinationOrigin = destinationOrigin + `/`
+}
+
+// Download site
 download({
 	entry: [
 		siteUrl,
