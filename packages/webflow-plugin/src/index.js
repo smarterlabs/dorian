6ba510f8d7aafa3397b12e85d76c80e5d11abfed
Kennedy Rose <kennedyianrose@gmail.com>
2021-08-05 13:29:40 -0500

    add lang attribute

--- packages/webflow-plugin/src/index.js
+++ packages/webflow-plugin/src/index.js
@@ -46,6 +46,11 @@ module.exports = function webflowPlugin(){
 			// const $head = $(`head`)
 			const $html = $(`html`)
 
+			// Add lang attrbute
+			if(!$html.attr(`lang`)){
+				$html.attr(`lang`, `en`)
+			}
+
 			// Polyfill for webp
 			if(useWebp){
 				$body.append(`<script>document.body.classList.remove('no-js');var i=new Image;i.onload=i.onerror=function(){document.body.classList.add(i.height==1?"webp":"no-webp")};i.src="data:image/webp;base64,UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==";</script>`)
